694b5e9bdce8badcd11e7a388e09ad1c0e733059
gabenn <52204063+gabenn@users.noreply.github.com>
2021-07-21 15:06:50 +0200

    feat: mobile navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,41 @@ import { default as HeaderStyles} from '../../styles/components/Header.module.sc
 const style = bemCssModules(HeaderStyles);
 
 const Header = () => {
+
+  //TODO
+  //WYCIĄGNĄĆ NAWIGACJE DO NOWEGO KOMPONENTU (PROBLEM ZE STYLAMI)
+  //ANIMACJA MENU MOBILE (MOŻE SAM CSS?)
+  const [showMenu, setShowMenu] = useState(false);
+  const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
+
+  const handleCloseMenu = ()=>setShowMenu(false)
+  const handleOpenMenu = ()=>setShowMenu(true)
+
+  const navigationMenu = !isMobile 
+    ? (
+      <nav className={style('navigation-bar')}>
+        <Link to='/'>STRONA GŁÓWNA</Link>
+        <Link to='/about'>O FIRMIE</Link>
+        <Link to='/offer'>OFERTA</Link>
+        <Link to='/contact'>KONTAKT</Link>
+      </nav>
+    )
+    : (
+      showMenu 
+      ?(
+      <nav className={`${style('mobile-navigation-bar')} `}>
+        <h1>MENU</h1>
+        <Link to='/'>STRONA GŁÓWNA</Link>
+        <Link to='/about'>O FIRMIE</Link>
+        <Link to='/offer'>OFERTA</Link>
+        <Link to='/contact'>KONTAKT</Link>
+        <button onClick={handleCloseMenu}>ZAMKNIJ</button>
+      </nav>
+      )
+      : <button className={style('mobile-show-button')} onClick={handleOpenMenu}>Pokaż menu</button>
+    )
+
+    
   //TO DO HAMBURGER
   return ( 
     <header className={style()}>
@@ -16,14 +51,10 @@ const Header = () => {
         LOGO
         {/* IMG */}
       </div>
-      <nav className={style('navigation-bar')}>
-        <Link to='/'>STRONA GŁÓWNA</Link>
-        <Link to='/about'>O FIRMIE</Link>
-        <Link to='/offer'>OFERTA</Link>
-        <Link to='/contact'>KONTAKT</Link>
-      </nav>
+      {navigationMenu}
     </header>
    );
+   
 }
  
 export default Header;
