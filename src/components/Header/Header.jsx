a350c2f9f8bd3d6180ffcaa3a2a1e7340ed74e9c
gabenn <52204063+gabenn@users.noreply.github.com>
2021-07-22 14:57:30 +0200

    feat: animation to menu MOBILE

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@
 import bemCssModules from 'bem-css-modules';
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useSpring, animated as a } from "react-spring";
 //Context
 //Components
 //Styles
@@ -13,11 +14,18 @@ const Header = () => {
   //TODO
   //WYCIĄGNĄĆ NAWIGACJE DO NOWEGO KOMPONENTU (PROBLEM ZE STYLAMI)
   //ANIMACJA MENU MOBILE (MOŻE SAM CSS?)
-  const [showMenu, setShowMenu] = useState(false);
   const isMobile = navigator.userAgent.toLowerCase().match(/mobile/i);
 
   const handleCloseMenu = ()=>setShowMenu(false)
   const handleOpenMenu = ()=>setShowMenu(true)
+
+  const [showMenu, displayMenu] = React.useState(false);
+  const contentProps = useSpring({
+    
+    opacity: showMenu ? 1 : 0,
+    marginLeft: showMenu ? 0 : -400,
+    transition: 1
+  });
 
   const navigationMenu = !isMobile 
     ? (
@@ -31,25 +39,24 @@ const Header = () => {
     : (
       showMenu 
       ?(
-      <nav className={`${style('mobile-navigation-bar')} `}>
+      <a.nav className={`${style('mobile-navigation-bar')} `} style={contentProps} >
         <h1>MENU</h1>
-        <Link to='/' onClick={handleCloseMenu}>STRONA GŁÓWNA</Link>
-        <Link to='/about' onClick={handleCloseMenu}>O FIRMIE</Link>
-        <Link to='/offer' onClick={handleCloseMenu}>OFERTA</Link>
-        <Link to='/contact' onClick={handleCloseMenu}>KONTAKT</Link>
-        <button onClick={handleCloseMenu}>ZAMKNIJ</button>
-      </nav>
+        <Link to='/' onClick={()=>displayMenu(false)}>STRONA GŁÓWNA</Link>
+        <Link to='/about' onClick={()=>displayMenu(false)}>O FIRMIE</Link>
+        <Link to='/offer' onClick={()=>displayMenu(false)}>OFERTA</Link>
+        <Link to='/contact' onClick={()=>displayMenu(false)}>KONTAKT</Link>
+        <button onClick={()=>displayMenu(false)}>ZAMKNIJ</button>
+      </a.nav>
       )
-      : <button className={style('mobile-show-button')} onClick={handleOpenMenu}>Pokaż menu</button>
+      : <button className={style('mobile-show-button')} onClick={()=>displayMenu(true)}>Pokaż menu</button>
     )
 
-    
   //TO DO HAMBURGER
   return ( 
     <header className={style()}>
       <div>
-        LOGO
-        {/* IMG */}
+        <img src='' alt="ALARMSYS" />
+        {/* //https://media.discordapp.net/attachments/855918865435328553/867440586785161236/logo.jpg?width=306&height=122 */}
       </div>
       {navigationMenu}
     </header>
