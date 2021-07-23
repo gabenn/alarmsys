c42db29bbc60a28c9a7f2d2182443932826d60a5
gabenn <52204063+gabenn@users.noreply.github.com>
2021-07-23 23:44:33 +0200

    feat: Footer Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './styles/components/App.module.scss';
+import './styles/App.module.scss';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Content from './components/Content/Content';
+
+
 
 const App = () => {
   return ( 
-    <div>
+    <>
       <Header />
-    </div>
+      <div className='content-wrapper'>
+        <Content />        
+      </div>
+      <Footer />
+    </>
    );
 }
  
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+//Libraries
+import bemCssModules from 'bem-css-modules';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+//Context
+//Components
+//Styles
+import { default as FooterStyles} from '../../styles/Footer.module.scss';
+const style = bemCssModules(FooterStyles);
+
+const Footer = () => {
+  return ( 
+    <footer className={style()}>
+      <address className={style('address')}>
+        <h3>Alarmsys Sławomir Cysewski</h3>
+        <span>ul. Morska 99/17</span>
+        <span>81-225 Gdynia</span>
+        <span>Telefon: +48 501-386-935</span>
+        <span><a href="mailto:alarmsys@alarmsys.pl">alarmsys@alarmsys.pl</a> </span>
+      </address>
+      <nav className={style('navigation-bar')}>
+        <Link to='/'>STRONA GŁÓWNA</Link>
+        <Link to='/about'>O FIRMIE</Link>
+        <Link to='/offer'>OFERTA</Link>
+        <Link to='/contact'>KONTAKT</Link>
+      </nav>
+    </footer>
+   );
+}
+ 
+export default Footer;
