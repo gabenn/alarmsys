151f00913169ced7c65a7a4011f013fb71eb31ec
gabenn <52204063+gabenn@users.noreply.github.com>
2021-07-24 11:48:32 +0200

    feat: content main panel work in proggress

--- src/Content/Main.jsx
+++ src/Content/Main.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+//Libraries
+import bemCssModules from 'bem-css-modules';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSpring, animated as a } from "react-spring";
+//Context
+//Components
+//Styles
+import { default as MainStyles} from '../styles/Main.module.scss';
+const style = bemCssModules(MainStyles);
 
 const Main = () => {
   return ( 
-    <div>
-      Main
-    </div>
+    <>
+      <div className={style('image-background')}>
+        <h1>Nasza firma jest koks zobacz niżej</h1>
+      </div>
+    </>
    );
-}
+};
  
 export default Main;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,14 +4,20 @@ import About from '../../Content/About';
 import Contact from '../../Content/Contact';
 import Main from '../../Content/Main';
 import Offer from '../../Content/Offer';
+//Styles
+import bemCssModules from 'bem-css-modules';
+import { default as ContentStyles} from '../../styles/Content.module.scss';
+const style = bemCssModules(ContentStyles);
 const Content = () => {
   return ( 
-    <Switch>
-      <Route exact path='/' render={Main} />
-      <Route exact path='/about' render={About} />
-      <Route exact path='/offer' render={Offer} />
-      <Route exact path='/contact' render={Contact} />
-    </Switch>
+    <main className={style()}>
+      <Switch>
+        <Route exact path='/' render={Main} />
+        <Route exact path='/about' render={About} />
+        <Route exact path='/offer' render={Offer} />
+        <Route exact path='/contact' render={Contact} />
+      </Switch>
+    </main>
    );
 }
  
