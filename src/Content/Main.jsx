0074ab84db507363cb7f535e8703ab4aa6851c0b
gabenn <52204063+gabenn@users.noreply.github.com>
2021-07-26 13:43:22 +0200

    feat: About module WIP

--- src/Content/Main.jsx
+++ src/Content/Main.jsx
@@ -11,11 +11,25 @@ const style = bemCssModules(MainStyles);
 
 const Main = () => {
   return ( 
-    <>
-      <div className={style('image-background')}>
+    <section className={style()}>
+      <section className={style('image-background')}>
         <h1>Nasza firma jest koks zobacz niżej</h1>
-      </div>
-    </>
+      </section>
+      <section className={style('about')}>
+        <div>
+          Firma ALARMSYS jest na rynku polskim firmą posiadającą dużą wiedzę i doświadczenie w zakresie oceny zagrożeń, projektowania i wykonywania wszelkiego rodzaju instalacji służących ochronie    obiektów, przebywających w nich ludzi i zgromadzonego mienia. W ciągu 4 lat działalności firma uczestniczyła w wykonawstwie systemów ochrony o różnym stopniu skomplikowania od najmniejszych   systemów dla mieszkań, biur ...  po systemy ochrony placówek banków, jednostek wojskowych, kancelarii tajnych i innych.
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </section>
    );
 };
  
